d23b1ce3118722f4088d45caa27f65b214f7589a
tkrivanek <tomislav.krivanek@neos.hr>
2019-02-16 23:01:23 +0100

    first one

---
+++ src/main/java/com/vvg/krivanek/warehouserental/WarehouseRentalApplication.java
@@ -0,0 +1,14 @@
+package com.vvg.krivanek.warehouserental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarehouseRentalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WarehouseRentalApplication.class, args);
+	}
+
+}
+
