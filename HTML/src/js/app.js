cbddb4267999c9920261d83c9c1ecbfd6a80b875
tkrivanek <tomislav.krivanek@neos.hr>
2020-04-29 10:20:33 +0200

    fragments and cleaning

--- HTML/src/js/app.js
+++
@@ -1,6 +0,0 @@
-// Dropdown
-$('.dropdown-toggle').click(function (e) {
-    e.preventDefault()
-    // $(this).next('.dropdown-menu').toggle();
-    $(this).parent('.dropdown').toggleClass('open');
-});
